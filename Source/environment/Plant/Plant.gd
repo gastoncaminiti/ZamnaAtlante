a0dc54d15871a6eb4b27917862ff000a8ecad7c9
gastoncaminiti <gastoncaminiti@outlook.com>
2019-06-25 18:40:01 -0300

    Código - Modo Edición III - Plantas
    *Se agrego la lógica para editar características de las plantas en modo edición:
    	-None (sin acción al iniciar).
    	-Origin Plant (Define la posición actual del sprite como la posición de crecimiento).
    	-Calibrate Plant (Limpia la planta de nodos hijos y ajusta su crecimiento en función de su posición).
    	-Clean(Limpia la planta de nodos hijos).
    -Refactor al calculo de unidad de desplazamiento de plantas.
    -Se agregaron comentarios aclaratorios en el script Plant.

--- Source/environment/Plant/Plant.gd
+++ Source/environment/Plant/Plant.gd
@@ -2,49 +2,48 @@ tool
 extends Node2D
 
 export(PackedScene)  var stem
-export(int) var length_stem setget set_leght_plant
+export(int,10) var length_plant
+export(int, "None", "Origin Plant","Calibrate Plant","Clean") var action_plant setget action_plant_editor
 export(Vector2) var origin_plant
-export(Vector2) var origin_grow 
-export(Vector2) var targetPoint
 
+onready var unit_move = global_position - $GrowPosition2D.global_position
+onready var canMove = false
+onready var isGrow  = false
+var origin_grow = Vector2(0,70)
 
-
-var canMove
-var isGrow
 
 
 func _ready():
-	connect_parent_child("plants_growed","grow")
-	connect_parent_child("plants_decreased","ingrow")
-	connect_parent_child("plants_stoped","timeEnd")
+	if not Engine.editor_hint:
+		connect_parent_child("plants_growed","grow")
+		connect_parent_child("plants_decreased","ingrow")
+		connect_parent_child("plants_stoped","timeEnd")
+	clean_plant()
 	generate_plant()
-	canMove = false
-	isGrow = false
 
+func _process(delta):
+	if(canMove):
+		if(isGrow):
+			global_position += unit_move * delta 
+		else:
+			global_position -= unit_move * delta 
 
+#--------Sección de manejo del crecimiento de la planta--------#
+#Función de generación de la planta en función del largo definido.
 func generate_plant():
-	global_position = origin_plant
-	$GrowPosition2D.position = origin_grow
-	for s in get_tree().get_nodes_in_group("Stem"):
-			s.queue_free()
-	for i in range(length_stem):
+	for i in range(length_plant):
 			addsteam(i)
-	global_position  -=  $GrowPosition2D.position / 2
-	
+	global_position  +=  unit_move * length_plant
 
-func addsteam(index):
+#Función que agrega un tronco a la planta.
+func addsteam(_index):
 	var new_stem = stem.instance()
 	new_stem.addone($GrowPosition2D)
 	new_stem.position = $GrowPosition2D.position
 	$GrowPosition2D.position += new_stem.base_point().position
 	add_child(new_stem)
-	print("Agregado el tronco "+str(index))
 
-func set_leght_plant(new_value):
-	length_stem = new_value
-	if(get_parent() != null):
-		generate_plant()
-
+#Función que controla el crecimiento de la planta sumando un tronco. / TRIGGER -> plants_growed
 func grow():
 	var new_stem = stem.instance()
 	new_stem.grow($GrowPosition2D)
@@ -53,6 +52,7 @@ func grow():
 	canMove = true
 	isGrow = true
 
+#Función que controla el decrecimiento de la planta restando un tronco. / TRIGGER -> plants_decreased
 func ingrow():
 	var last_stem = get_children().back()
 	if(last_stem.is_in_group("Stem")):
@@ -62,14 +62,39 @@ func ingrow():
 		canMove = true
 		isGrow = false
 
-func _process(delta):
-	
-	if(canMove):
-		if(isGrow):
-			global_position += targetPoint * delta 
-		else:
-			global_position -= targetPoint * delta 
+#--------Sección de control de estados--------#
+#Función SET que acciona el comportamiento de la planta en el modo edición.
+func action_plant_editor(new_value):
+	if Engine.editor_hint:
+		action_plant = new_value
+		match action_plant:
+			0:
+				print("Sin acción")
+			1:
+				print("Definir posición actual como punto de crecimiento")
+				set_origin_plant()
+			2:
+				print("Calibrado crecimiento a posición origen")
+				clean_plant()
+				unit_move = global_position - $GrowPosition2D.global_position
+				generate_plant()
+			3:
+				print("Limpiar los troncos")
+				clean_plant()
 
+#Función que reinicia la planta a su estado inicial.
+func clean_plant():
+	global_position = origin_plant
+	$GrowPosition2D.position = origin_grow
+	for i in get_children():
+		if(i.is_in_group("Stem")):
+    		i.queue_free()
+	
+#Función que reinicia la planta a su estado inicial.
+func set_origin_plant():
+	origin_plant = global_position
+	
+#Función TRIGGER -> plants_stoped
 func timeEnd():
 	canMove = false
 	
